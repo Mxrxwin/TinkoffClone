b486e7fab1916ace98e917cb3f5f6945aa31dba0
Некита <n_bronivec@mail.ru>
2024-01-18 16:50:28 +0300

    Created a new Expo app

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import * as Font from "expo-font";
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+    const loadFonts = async () => {
+      await Font.loadAsync({
+        tbold: require("./assets/fonts/TinkoffSansBold.ttf"),
+        tmedium: require("./assets/fonts/TinkoffSansMedium.ttf"),
+        tregular: require("./assets/fonts/TinkoffSansRegular.ttf")
+      });
+    };
+
+    useEffect(() => {
+      const loadFontAsync = async () => {
+        await loadFonts();
+        setFontLoaded(true);
+      };
+  
+      loadFontAsync();
+    }, []);
+  
+    if (!fontLoaded) {
+      return null; 
+    }
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontFamily: 'tregular', color: 'white'}}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +39,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#121212',
     alignItems: 'center',
     justifyContent: 'center',
   },
